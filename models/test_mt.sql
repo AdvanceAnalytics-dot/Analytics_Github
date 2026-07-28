92552753e32bdd85de2fb664f086bbe4d3506673
BuddiReddy Murali Raju <muralicloudengineer@gmail.com>
2026-07-28 11:11:06 +0000

    alias names

--- models/test_mt.sql
+++ models/test_mt.sql
@@ -1,4 +1,6 @@
 {{ config(materialized= 'table') }}
 select
-*
-from ANALYTICSDBT.DATAENGINEER_TEST.mouritech
+name as emp_name,
+role as emp_role,
+mobile_number as contact
+from {{ source( 'DATAENGINEER_TEST'.'mouritech') }}
