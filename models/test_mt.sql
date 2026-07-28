8ceaba9aa768a631ca6661efc18b480fbff0ab60
BuddiReddy Murali Raju <muralicloudengineer@gmail.com>
2026-07-28 11:23:24 +0000

    test

--- models/test_mt.sql
+++ models/test_mt.sql
@@ -2,6 +2,6 @@
 select
 name as emp_name,
 role as emp_role,
-mobile_number as contact
+mobile_number as contact,
 case when contact = '1232415112' then '91' else '' end as code, 
 from ANALYTICSDBT.DATAENGINEER_TEST.MOURITECH
