0f4dbea7f89d2501fcbb8c20d28119ec8630977c
BuddiReddy Murali Raju <muralicloudengineer@gmail.com>
2026-07-28 11:21:34 +0000

    code change

--- models/test_mt.sql
+++ models/test_mt.sql
@@ -3,4 +3,5 @@ select
 name as emp_name,
 role as emp_role,
 mobile_number as contact
+case when contact = '1232415112' then '91' else '' end as code, 
 from ANALYTICSDBT.DATAENGINEER_TEST.MOURITECH
