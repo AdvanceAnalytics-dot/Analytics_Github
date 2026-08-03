3a408230e3bb57223c24dbc5ed33aef6d5ba7359
BuddiReddy Murali Raju <muralicloudengineer@gmail.com>
2026-08-03 07:16:59 +0000

    sanpshot3

--- snapshots/dev_raw_cust.sql
+++ snapshots/dev_raw_cust.sql
@@ -4,10 +4,11 @@
 config(
         unique_key='CUST_NUMBER',
         strategy='timestamp',
-        update_at='create_date'
+        updated_at='last_updated'
 )
 }}
 select*
-from{{ source( 'DEV_RAW','DEV_CUST') }}
+from
+{{ source( 'DEV_RAW','DEV_CUST') }}
 
 {% endsnapshot %}
